178a02ff051c7a16b711c4d01d20aa230f971369
Misha <misharahnema@gmail.com>
2025-01-27 16:24:55 +0100

    Logo et background page login signup + Page login en cours

---
+++ app/src/main/java/com/example/projetb3_mdp_online/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.projetb3_mdp_online.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
