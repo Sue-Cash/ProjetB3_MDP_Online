b68b37d3d6f47823a5b89e9a04e8a149bb8fbe31
Misha <misharahnema@gmail.com>
2025-03-31 13:49:33 +0200

    Page PS Groupe en Cours, Page All password en cours, Page sign up terminé

--- app/src/main/java/com/example/projetb3_mdp_online/PasswordGroupesActivity.kt
+++ app/src/main/java/com/example/projetb3_mdp_online/PasswordGroupesActivity.kt
@@ -3,10 +3,10 @@ package com.example.projetb3_mdp_online
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SignUpActivity : AppCompatActivity() {
+class PasswordGroupesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // This tells the activity to use activity_signup.xml as its layout
-        setContentView(R.layout.activity_signup)
+        // This tells the activity to use activity_login.xml as its layout
+        setContentView(R.layout.activity_password_groupes)
     }
 }
