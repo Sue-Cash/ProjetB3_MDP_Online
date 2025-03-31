7478d07de922e3f19e65fc2c58f8165763b32740
Misha <misharahnema@gmail.com>
2025-03-31 12:03:07 +0200

    Logo et background page login signup + Page login avec Redirection depuis activity main

--- app/src/main/java/com/example/projetb3_mdp_online/MainActivity.kt
+++ app/src/main/java/com/example/projetb3_mdp_online/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.projetb3_mdp_online
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Find the sign up button and set an onClickListener
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+        btnSignUp.setOnClickListener {
+            // Create an intent to start SignupActivity
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Find the sign up button and set an onClickListener
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            // Create an intent to start LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
