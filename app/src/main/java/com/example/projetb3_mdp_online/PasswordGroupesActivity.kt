b0b0b9700a3a961e3ba8ec2dbbdcbd30b32c9d65
Misha <misharahnema@gmail.com>
2025-04-13 18:53:14 +0200

    Front page affichage MDP

--- app/src/main/java/com/example/projetb3_mdp_online/PasswordGroupesActivity.kt
+++ app/src/main/java/com/example/projetb3_mdp_online/PasswordGroupesActivity.kt
@@ -1,20 +1,28 @@
 package com.example.projetb3_mdp_online
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class PasswordGroupesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // This tells the activity to use activity_login.xml as its layout
         setContentView(R.layout.activity_password_groupes)
 
-
         val backButton = findViewById<ImageButton>(R.id.backButton)
-
         backButton.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        // 1) Find the layout for "All"
+        val cardAll = findViewById<LinearLayout>(R.id.passwordAll)
+
+        // 2) Set a click listener to navigate to PasswordAllActivity
+        cardAll.setOnClickListener {
+            val intent = Intent(this, PasswordAllActivity::class.java)
+            startActivity(intent)
         }
     }
 }
