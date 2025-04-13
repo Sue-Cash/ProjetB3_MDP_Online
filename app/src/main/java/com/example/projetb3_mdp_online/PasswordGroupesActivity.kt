3800593e41b5c8ec2a02bf177f4a3b472a311d9b
Misha <misharahnema@gmail.com>
2025-04-13 14:59:21 +0200

    Page 4 Groupe MDP Créer + Redirection Back mais pas dans les boutons

---
+++ app/src/main/java/com/example/projetb3_mdp_online/PasswordGroupesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projetb3_mdp_online
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PasswordGroupesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This tells the activity to use activity_login.xml as its layout
+        setContentView(R.layout.activity_password_groupes)
+    }
+}
---
+++ app/src/main/java/com/example/projetb3_mdp_online/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projetb3_mdp_online
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This tells the activity to use activity_signup.xml as its layout
+        setContentView(R.layout.activity_signup)
+    }
+}
